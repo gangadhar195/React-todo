6bb2cf2af55aa8fcec696f78ab31638bb18b1076
gangadhar195 <gangadhars9545@gmail.com>
2025-02-25 12:06:45 +0530

    add some

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
 import "./App.css"
 import { useState } from 'react'
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 const App = () => {
   const [item,setItem] = useState([]);
-
-  return (
+  const[input,setInput]=useState();
+  const addtodo =()=>{
+    setItem([...item,input])
+  }
+  console.log(item)
+  return ( 
     <div className='todo'>
       <div>
       <h1>Todo List</h1>
-      <input type='text' placeholder='Add todo' onChange={(e)=>setItem(e.target.value)} name=''/>
-      <button>Add Todo</button>
+      <div className="inputdata">
+      <input type='text' placeholder='Add todo' onChange={(e)=>setInput(e.target.value)} value={input}/>
+      <button onClick={()=>
+      {addtodo(input) 
+        setInput("")}} >Add Todo</button>
+        </div>
       </div>
       <div className="list">
-        <li>{item}</li>
+       {item.map((n,i)=>(
+
+         <li key={i}>{n} <span><RiDeleteBin6Line /></span></li>
+       ))}
       </div>
     </div>
   )
